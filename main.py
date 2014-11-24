e82e39e8b9355c63ba77565fc179b8a9194b0874
tianyaqu <tianyaqu@qq.com>
2014-11-24 21:38:43 +0800

    sperate template

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+import re
+import os.path
+from pymongo import MongoClient
 import tornado
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import re
-import os.path
 from tornado.options import define,options
 
 define("port",default=8888,help="run on a given port",type=int)
@@ -21,11 +22,13 @@ class APP(tornado.web.Application):
         settings = dict(
             template_path = os.path.join(os.path.dirname(__file__),"templates"),
             static_path = os.path.join(os.path.dirname(__file__),"static"),
+            debug = True,
         )
 
         tornado.web.Application.__init__(self,handlers,**settings)
         #connect to mogodb
-        self.db = None
+        client = MongoClient()
+        self.db = client['torrent']
 
 class BaseHandler(tornado.web.RequestHandler):
     @property
@@ -36,17 +39,23 @@ class BaseHandler(tornado.web.RequestHandler):
 class HomeHandler(BaseHandler):
     def get(self):
         self.render(
-            "index.html",
+            "home.html",
             title = "TV torrent | Home",
             text = "Welcome to TV torrent!",
         )
 
 class CategoryHandler(BaseHandler):
     def get(self):
+        btList = self.db.priate.find() 
+        xbtList = []
+        for i in btList:
+            xbtList.append(i)
+        ybtList = xbtList[:20]
         self.render(
             "category.html",
             title = "category header",
             text = "hello caome",
+            btList = ybtList,
         )
 
 def main():
